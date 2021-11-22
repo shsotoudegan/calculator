b1e0a4b471d9ca23fb2380daf2fe70d7e3ff93ef
shsotoudegan <shsotoudegan@gmail.com>
2021-11-22 14:52:52 +0330

    Fix buggs

--- cli/cli_calculator.py
+++ cli/cli_calculator.py
@@ -11,30 +11,30 @@ inputs = sys.argv
 command = inputs[1].lower()
 number_1 = float(inputs[2])
 number_2 = float(inputs[3])
- 
+
 
 # use "if statment" to check which command is entred by user
 if command in ["+", "add", "a", "-a"]:
-	print(f"{number_1} + {number_2} = {number_1 + number_2}")
+    print(f"{number_1} + {number_2} = {number_1 + number_2}")
 
 elif command in ["-", "minus", "mn", "-mn"]:
-	print(f"{number_1} - {number_2} = {number_1 - number_2}")
-	print(f"{number_2} - {number_1} = {number_2 - number_1}")
+    print(f"{number_1} - {number_2} = {number_1 - number_2}")
+    print(f"{number_2} - {number_1} = {number_2 - number_1}")
 
-elif command in ["multiplicaation", "ml", "-ml"]: # * operator is meaningfull in cli
-	print(f"{number_1} * {number_2} = {number_1 * number_2}")
+elif command in ["multiplicaation", "ml", "-ml"]:  # * operator is meaningfull in cli
+    print(f"{number_1} * {number_2} = {number_1 * number_2}")
 
 elif command in ["/", "dvision", "d", "-d"]:
-	print(f"{number_1} / {number_2} = {number_1 / number_2}")
-	print(f"{number_2} / {number_1} = {number_2 / number_1}")
+    print(f"{number_1} / {number_2} = {number_1 / number_2}")
+    print(f"{number_2} / {number_1} = {number_2 / number_1}")
 
 elif command in ["%", "eamaining", "r", "-r"]:
-	print(f"{number_1} % {number_2} = {number_1 % number_2}")
-	print(f"{number_2} % {number_1} = {number_2 % number_1}")
+    print(f"{number_1} % {number_2} = {number_1 % number_2}")
+    print(f"{number_2} % {number_1} = {number_2 % number_1}")
 
-elif command in ["power", "p", "-p"]: # ** operator is meaningfull in cli
-	print(f"{number_1} ** {number_2} = {number_1 ** number_2}")
-	print(f"{number_2} ** {number_1} = {number_2 ** number_1}")
+elif command in ["power", "p", "-p"]:  # ** operator is meaningfull in cli
+    print(f"{number_1} ** {number_2} = {number_1 ** number_2}")
+    print(f"{number_2} ** {number_1} = {number_2 ** number_1}")
 
 else:
-	print(f"{command} {number_1} {number_2} => error")
+    print(f"{command} {number_1} {number_2} => error")
